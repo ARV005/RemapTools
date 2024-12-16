d1d1811e43dd1abe52bc6ee2b080887442d73001
ARV005 <ravilev7@gmail.com>
2024-12-16 19:37:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const password = document.getElementById('fakePassword');
+const toggler = document.getElementById('toggler');
+showHidePassword = () => {
+if (password.type == 'password') {
+password.setAttribute('type', 'text');
+toggler.classList.add('fa-eye-slash');
+} else {
+toggler.classList.remove('fa-eye-slash');
+password.setAttribute('type', 'password');
+}
+};
+toggler.addEventListener('click', showHidePassword);
